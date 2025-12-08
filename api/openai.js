42f9c336a46026302de5dc9f334a428b61cbdbd4
Daniel Nóbrega <contatonsdaniel@gmail.com>
2025-12-08 16:37:29 -0300

    feat: api finally acting like a nutritional agent ;integrating the api into a html

--- api/openai.js
+++ api/openai.js
@@ -6,14 +6,14 @@ export const client = new OpenAI({
   apiKey: process.env.OPENAI_API_KEY
 });
 
-export async function gerarResposta(prompt) {
-  const resposta = await client.responses.create({
+export async function generateAnswer(user_prompt) {
+  const answer = await client.responses.create({
     model: "gpt-4o-mini",
-    input: prompt
+    instructions: 'Você é um nutricionista especializado em análise de consumo alimentar. Sua tarefa é receber alimentos informados pelo usuário e estimar calorias e proteínas totais usando valores médios de porções comuns (por exemplo: cuscuz = 1 prato médio, frango = 100 g, ovo = 1 unidade, arroz = 1 concha). Nunca utilize porções exageradas ou irreais. Sempre retorne exatamente o seguinte formato, sem explicações adicionais: "Total de kcal consumidas: X" e "Total de proteínas consumidas: Y g".',
+    input: user_prompt
   });
 
-  // NOVO FORMATO DA API:
-  const texto = resposta.output[0].content[0].text;
+  const result_answer = answer.output[0].content[0].text;
 
-  return texto;
+  return result_answer;
 }
--- api/server.js
+++ api/server.js
@@ -1,22 +1,29 @@
-import express, {json} from "express";
-import { gerarResposta } from "./openai.js";
+import express from "express";
+import { generateAnswer } from "./openai.js";
+import path from "path";
+import { fileURLToPath } from "url";
+
 
 const app = express();
 const port = 3000;
 
+const __filename = fileURLToPath(import.meta.url);
+const __dirname = path.dirname(__filename);
+
+app.use(express.static(path.join(__dirname, "public")));
 app.use(express.json());
 
-app.post("/api/teste", async (req, res) => {
+app.post("/api/dietai", async (req, res) => {
     try {
-        const { prompt } = req.body;
-        const resposta = await gerarResposta(prompt);
+        const { user_prompt } = req.body;
+        const answer = await generateAnswer(user_prompt);
         res.status(200).json({
-            recebido: prompt,
-            resposta
+            question: user_prompt,
+            answer
         });
     } catch (err) {
         console.error(err);
-        res.status(500).json({error: "Erro interno."});
+        res.status(500).json({error: "Internal error."});
     }
 });
 
